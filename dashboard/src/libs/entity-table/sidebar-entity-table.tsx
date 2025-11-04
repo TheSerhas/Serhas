99d89102572ccde57039a543c7137828486749b2
erfjab <erfjab@gmail.com>
2025-11-05 02:34:14 +0330

    imp hosts

--- dashboard/src/libs/entity-table/sidebar-entity-table.tsx
+++ dashboard/src/libs/entity-table/sidebar-entity-table.tsx
@@ -1,4 +1,4 @@
-import { Button, ResizableHandle, ResizablePanel, ResizablePanelGroup, HStack } from "@serhas/common/components";
+import { Button, HStack } from "@serhas/common/components";
 import { Table } from "@tanstack/react-table"
 import { DataTableViewOptions } from "./components";
 import { Server } from "lucide-react";
@@ -9,7 +9,6 @@ import {
     DataTablePagination,
     EntityDataTable,
     SidebarEntityTablePopover,
-    SidebarEntitySelection
 } from "./components";
 import {
     useSidebarEntityTable,
@@ -48,7 +47,6 @@ function MainTable<T, S>({ table, columns, props }: { table: Table<T>, columns: 
 export function SidebarEntityTable<T, S>(props: SidebarEntityTableProps<T, S>) {
     const { t } = useTranslation();
     const {
-        desktop,
         table,
         entityTableContextValue,
         sidebarEntityTableContextValue,
@@ -62,15 +60,13 @@ export function SidebarEntityTable<T, S>(props: SidebarEntityTableProps<T, S>) {
                 <div className="flex flex-col">
                     <div className="flex flex-col md:flex-row-reverse items-center py-4 gap-2 w-full">
                         <div className="flex flex-row justify-center items-center w-full">
-                            {!desktop && (
-                                <SidebarEntityTablePopover
-                                    buttonChild={
-                                        <HStack className="items-center p-1">
-                                            <Server className="size-4" /> {t("inbounds")}
-                                        </HStack>
-                                    }
-                                />
-                            )}
+                            <SidebarEntityTablePopover
+                                buttonChild={
+                                    <HStack className="items-center p-1">
+                                        <Server className="size-4" /> {t("inbounds")}
+                                    </HStack>
+                                }
+                            />
                             <DataTableViewOptions table={table} />
                             {onCreate && (
                                 <Button aria-label={`create-${props.entityKey}`} onClick={onCreate}>
@@ -81,19 +77,7 @@ export function SidebarEntityTable<T, S>(props: SidebarEntityTableProps<T, S>) {
                         <TableSearch />
                     </div>
                     <div className="w-full rounded-md border">
-                        {desktop ? (
-                            <ResizablePanelGroup direction="horizontal">
-                                <ResizablePanel minSize={20} maxSize={40}>
-                                    <SidebarEntitySelection />
-                                </ResizablePanel>
-                                <ResizableHandle withHandle={desktop} />
-                                <ResizablePanel >
-                                    <MainTable table={table} columns={columns} props={props} />
-                                </ResizablePanel>
-                            </ResizablePanelGroup>
-                        ) : (
-                            <MainTable table={table} columns={columns} props={props} />
-                        )}
+                        <MainTable table={table} columns={columns} props={props} />
                     </div>
                 </div>
             </SidebarEntityTableContext.Provider>
